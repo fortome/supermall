8373d757a67ce1247df34e44dbfee8bcf4ff513b
fortome <1160051509@qq.com>
2021-11-28 15:46:43 +0800

    第三次初始化项目

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="item in goods" :goods-item="item"/>
+  </div>
+</template>
+
+<script>
+  import GoodsListItem from "./GoodsListItem";
+
+  export default {
+    name: "GoodsList",
+    components: {
+      GoodsListItem
+    },
+    props: {
+      goods: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    /* flex-wrap默认是 nowrap不包裹，就会排成一行，而flex-wrap: wrap;是根据设置的孩子的宽度来包裹，就可以分行*/
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 2px;
+
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,47 +1,171 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"/>
+      <goods-list :goods="showGoods"/>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
 <script>
   // 只要用default导出的才不用加{}
-  import NavBar from "components/common/navbar/NavBar";
+
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
 
-  import {getHomeMultidata} from "network/home";
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
+  import Scroll from "components/common/scroll/Scroll";
+  import BackTop from "components/content/backTop/BackTop"
+
+  import {getHomeMultidata, getHomeGoods} from "network/home";
   export default {
     name: "Home",
     components: {
-      NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+
+      NavBar,
+      TabControl,
+      GoodsList,
+      Scroll,
+      BackTop
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
+        currentType: 'pop',
+        isShowBackTop: false
+      }
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
       }
     },
     // 组件创建完成发送网络请求
     created() {
       // 1.请求多个数据
-      getHomeMultidata().then(res => {
-        // console.log(res);
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      // this.getHomeMultidata() 调用methods里的getHomeMultidata()方法
+      // getHomeMultidata() 是直接调用import {getHomeMultidata, getHomeGoods} from "network/home";导入的getHomeMultidata()方法
+      this.getHomeMultidata()
+
+      // 2.请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      /**
+       * 事件监听相关方法
+       * */
+      tabClick(index) {
+        // console.log(index);
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+      },
+      backClick() {
+        // 拿到scroll组件对象：this.$refs.scroll
+        this.$refs.scroll.scrollTo(0, 0)
+      },
+      contentScroll(position) {
+        // console.log(position);
+        this.isShowBackTop = ((-position.y) > 1000)
+      },
+      loadMore() {
+        // console.log('上拉加载');
+        this.getHomeGoods(this.currentType)
+      },
+      /**
+       * 网络请求的相关方法
+       * */
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          // console.log(res);
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then(res => {
+          // console.log(res);
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1
+
+          this.$refs.scroll.finishPullUp()
+        })
+      }
     }
 
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+    height: 100vh;
+    position: relative;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
-    color: var(--color-background)
+    color: var(--color-background);
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
+   /*position: sticky; 滑到某个地方（top: 44px）固定住, sticky:粘性的*/
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+
+  }
+
+  /*.content {*/
+  /*  height: calc(100% - 93px);*/
+  /*  overflow: hidden;*/
+  /*  margin-top: 44px;*/
+  /*}*/
 </style>
